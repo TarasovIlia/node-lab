cc70d399098abe4d2a5888978a1cf035378b2d3a
Tarasov_Ilya <tarasov002@gmail.com>
2021-09-10 16:34:04 +0300

    basic http server

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-const http = require
+const http = require('http')
+const port = 4200
+
+const requestHandler = (req, res) => {
+    console.log(req.url)
+    res.end('My Server')
+}
+
+
+const localUrl = path => path ?  `http://localhost:${port}/${path}` : `http://localhost:${port}`
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+    if (err) {
+        return console.log
+    }
+    console.log(`server is listening on ${localUrl()}`)
+})
