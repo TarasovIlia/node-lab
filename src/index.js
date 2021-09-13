3a4d9b5dcdc9789110ef56179b61cc43266e1f49
Tarasov_Ilya <tarasov002@gmail.com>
2021-09-13 14:16:37 +0300

    add mongodb database

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+
+
 const products = require('./products');
 const app = express();
 const os = require('os');
+const mongoose = require('mongoose')
 
 const name = process.env.USERNAME.replace('.',' ');
-const cpus = os.cpus().length
+const cpus = os.cpus().length;
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+});
 
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
 
 const host = 'http://localhost:'
-const port = 4200
+const port = process.env.PORT || 4200
 const localUrl = url => url ?  `${host}${port}/${url}` : `${host}${port}`
 
 app.get('/products', (req,res) => {
@@ -19,10 +30,17 @@ app.get('/', (req,res) => {
     res.send(`<h2>Hello ${name}<h2/>`);
 })
 
-
-app.listen(port, (err) => {
-    if (err) {
-        return console.log("somethings bad happened", err)
+async function start() {
+    try {
+        await mongoose.connect('mongodb+srv://IliyaTarasav:1oviver1@cluster0.esyae.mongodb.net/products', {
+            useNewUrlParser: true,
+        })
+        app.listen(port, () => {
+            console.log(`\n\nserver is listening on ${localUrl()}\n\nserver is listening on ${localUrl('products')}`)
+        })
+    } catch(err) {
+        console.log(err)
     }
-    console.log(`\n\nserver is listening on ${localUrl()}\n\nserver is listening on ${localUrl('products')}`)
-})
+}
+
+start()
