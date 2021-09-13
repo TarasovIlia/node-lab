690065b0d4367bb0c8ae0b98d0059c4645c13524
Tarasov_Ilya <tarasov002@gmail.com>
2021-09-13 14:57:13 +0300

    connect database

--- src/index.js
+++ src/index.js
@@ -1,30 +1,21 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
+//const exphbs = require('express-handlebars');
+const { MongoClient } = require('mongodb')
 
 
 const products = require('./products');
 const app = express();
 const os = require('os');
-const mongoose = require('mongoose')
 
 const name = process.env.USERNAME.replace('.',' ');
 const cpus = os.cpus().length;
-const hbs = exphbs.create({
-    defaultLayout: 'main',
-    extname: 'hbs'
-});
-
-app.engine('hbs', hbs.engine)
-app.set('view engine', 'hbs')
-app.set('views', 'views')
 
 const host = 'http://localhost:'
 const port = process.env.PORT || 4200
 const localUrl = url => url ?  `${host}${port}/${url}` : `${host}${port}`
 
-app.get('/products', (req,res) => {
-    res.send(products);
-})
+const client = new MongoClient('mongodb+srv://IliyaTarasav:1oviver1@cluster0.esyae.mongodb.net/products')
+
 
 app.get('/', (req,res) => {
     res.send(`<h2>Hello ${name}<h2/>`);
@@ -32,9 +23,12 @@ app.get('/', (req,res) => {
 
 async function start() {
     try {
-        await mongoose.connect('mongodb+srv://IliyaTarasav:1oviver1@cluster0.esyae.mongodb.net/products', {
-            useNewUrlParser: true,
-        })
+        await client.connect()
+        console.log('Connected');
+        const products = client.db().collection('products');
+        await products.insertOne({displayName:"God Of War", price:50});
+        const game = await products.findOne({displayName:'God Of War'})
+        console.log(game )
         app.listen(port, () => {
             console.log(`\n\nserver is listening on ${localUrl()}\n\nserver is listening on ${localUrl('products')}`)
         })
