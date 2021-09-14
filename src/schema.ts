75708b01aa80d1cbe16ad450d1551ce4cc7b3fa8
Tarasov_Ilya <tarasov002@gmail.com>
2021-09-14 15:59:35 +0300

    create interface

--- src/schema.ts
+++ src/schema.ts
@@ -1,11 +1,23 @@
-const mongoose = require('mongoose')
+import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
+interface Category {
+    displayName: String,
+    createdAt: Date,
+}
 
 const categorySchema = new Schema({
     displayName: String,
     createdAt: Date,
 })
+
+interface Production {
+    displayName: String,
+    category: Category,
+    createdAt: Date,
+    totalRating: Number,
+    price: Number,
+}
 
 const productSchema = new Schema({
     displayName: String,
--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,17 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const fs = require('fs') 
 const path = require('path');
 
-// fs.readdirSync('node_modules')
-// .filter(x => ['.bin'].indexOf(x) === -1)
-// .forEach(mod => nodeModules[mod] = 'comminjs ' + mod)
 
 const isDev = process.env.NODE_ENV === 'development'
 
 const filename = ext => isDev ? `[name].${ext}` : `[name].${ext}`;
 
 const nodeModules = {};
+
 fs.readdirSync('node_modules')
-  .filter(function(x) {
-    return ['.bin'].indexOf(x) === -1;
-})
-  .forEach(function(mod) {
-    nodeModules[mod] = 'commonjs ' + mod;
-});
+  .filter(function(x) { return ['.bin'].indexOf(x) === -1;})
+  .forEach(function(mod) {nodeModules[mod] = 'commonjs ' + mod;});
+
 module.exports = {
     context: path.resolve(__dirname, './src'),
     entry: './main',
