19838fd11b50c22e96103680df941f7cafee0e44
Tarasov_Ilya <tarasov002@gmail.com>
2021-09-15 08:35:00 +0300

    install and config prettier

--- src/schema.ts
+++ src/schema.ts
@@ -2,32 +2,32 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 interface Category {
-    displayName: String,
-    createdAt: Date,
+  displayName: String;
+  createdAt: Date;
 }
 
 const categorySchema = new Schema({
-    displayName: String,
-    createdAt: Date,
-})
+  displayName: String,
+  createdAt: Date,
+});
 
 interface Production {
-    displayName: String,
-    category: Category,
-    createdAt: Date,
-    totalRating: Number,
-    price: Number,
+  displayName: String;
+  category: Category;
+  createdAt: Date;
+  totalRating: Number;
+  price: Number;
 }
 
 const productSchema = new Schema({
-    displayName: String,
-    category: categorySchema,
-    createdAt: Date,
-    totalRating: Number,
-    price: Number,
-})
+  displayName: String,
+  category: categorySchema,
+  createdAt: Date,
+  totalRating: Number,
+  price: Number,
+});
 
-const product = mongoose.model('products', productSchema)
-const category = mongoose.model('catergry', categorySchema)
+const product = mongoose.model('products', productSchema);
+const category = mongoose.model('catergry', categorySchema);
 
-module.exports = product, category;
+(module.exports = product), category;
