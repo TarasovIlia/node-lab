0ab79cf0974535dadc2273b92b4a17f9f8533295
Tarasov_Ilya <tarasov002@gmail.com>
2021-09-16 12:17:17 +0300

    add config file

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import mongoose from 'mongoose'
 const app = express()
 const Product = require('./schema')
 const Category = require('./schema')
+const config = require('config')
 
-const host = 'http://localhost:'
-const port = process.env.PORT || 4200
+const HOST = config.get('host')
+const PORT = config.get('post') || 4200
 
-const localUrl = (url: String) => (url ? `${host}${port}/${url}` : `${host}${port}`)
+const localUrl = (url: String) => (url ? `${HOST}${PORT}/${url}` : `${HOST}${PORT}`)
 
 const newGame = new Product({
   displayName: 'test',
@@ -22,14 +23,14 @@ async function start () {
     await mongoose.connect('mongodb+srv://IliyaTarasav:1oviver1@cluster0.esyae.mongodb.net/products')
     console.log('\n\nConnected')
     const game = await Product.find({})
-    await newGame.save()
+    //await newGame.save()
     app.get('/', (req, res) => {
       res.send(`<a href=${localUrl('products')}>to products<a>`)
     })
     app.get('/products', (req, res) => {
       res.send(`<h2>All game: ${game[0].displayName}<h2/>`)
     })
-    app.listen(port, () => {
+    app.listen(PORT, () => {
       console.log(`\n\nserver is listening on ${localUrl('')}\n\nserver is listening on ${localUrl('products')}`)
     })
   } catch (err) {
