e3cfb24dabfa458afc1aa1385952b0140b108b58
Tarasov_Ilya <tarasov002@gmail.com>
2021-09-28 10:09:33 +0300

    9-logs

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,12 @@ const HOST = config.get('host')
 const PORT = config.get('port') || 4200
 const localUrl = (url: String) => (url ? `${HOST}${PORT}/${url}` : `${HOST}${PORT}`)
 
-const game = new Games()
-game.display_name = 'The Witcher';
-game.price = 56;
-game.total_rating = 10;
-game.category = 'Action';
-game.create_at = new Date(2013,3,3);
+// const game = new Games()
+// game.display_name = 'The Witcher';
+// game.price = 56;
+// game.total_rating = 10;
+// game.category = 'Action';
+// game.create_at = new Date(2013,3,3);
 
 async function start() {
   try {
@@ -34,17 +34,16 @@ async function start() {
     .then ( async connection => {
         const gamesRepository = connection.getRepository(Games)
       
-        await gamesRepository.save(game)
         const allGame = await gamesRepository.find()
         console.log(allGame)
       
         
         app.listen(PORT, () => console.log(`\n\nserver run on port ${localUrl('')}`))
       })
-      
+
   } catch (error) {
     console.log(error)
-  }
+    } 
 }
 
 start()
