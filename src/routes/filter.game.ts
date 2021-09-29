427b004e3182faa80cfcdf7a66cd9fed6772176c
Tarasov_Ilya <tarasov002@gmail.com>
2021-09-29 14:17:21 +0300

    pagination

--- src/routes/filter.game.ts
+++ src/routes/filter.game.ts
@@ -8,8 +8,6 @@ router.get('/filter', async function(req,res) {
     try {
         //const {target} = req.body
         const target = {hig : 40, min : 30}
-        // const params = typeof target === 'number' ? 'id' : 'display_name'
-        // const creteria = typeof target === 'number' ? {id : target} : {display_name : target}
         const game = await getRepository(Games)
             .createQueryBuilder('games')
             .where(`games.price > :minPrice`, {minPrice : target.min} )
--- src/routes/get.all.game.ts
+++ src/routes/get.all.game.ts
@@ -6,8 +6,13 @@ const router = Router()
 
 router.get('/get/all', async function(req,res) {
     try {
+        const page = 1
+        const PAGE_SIZE = 10;
+        const skip = (page - 1) * PAGE_SIZE;
         const gameRepository = await getRepository(Games)
             .createQueryBuilder('games')
+            .take(PAGE_SIZE)
+            .skip(skip)
             .getMany();
         res.send(gameRepository)
     } catch (error) {
